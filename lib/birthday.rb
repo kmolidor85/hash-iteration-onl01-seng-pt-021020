9fa1032278714cbd7dc250ea35978f0b6f2313fe
kyle molidor <k_molidor@yahoo.com>
2020-02-23 08:08:30 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -4,11 +4,10 @@
 #   "Amanda" => 27
 # }
 
-def happy_birthday(birthday_kids)
   # add your code snippet here!
-  def happy_birthday(birthday_kids)
-  birthday_kids.each do |kids_name, age|
-    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+def happy_birthday(birthday_kids)
+  birthday_kids.each do |name, age|
+    puts "Happy Birthday #{name}! You are now #{age} years old!"
   end
 end
 end
